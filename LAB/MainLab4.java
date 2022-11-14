4a2a1d4d8185ea7fee54ec98ab5d759b4474bdc5
KacperPacuraW65559 <115528912+KacperPacuraW65559@users.noreply.github.com>
2022-11-14 20:39:12 +0100

    Update MainLab4.java

--- LAB/MainLab4.java
+++ LAB/MainLab4.java
@@ -4,18 +4,17 @@ import java.time.LocalDate;
 public class MainLab4 {
     public static void main(String[] args) {
 
-//      ProstokatP1 p1 = new ProstokatP1(2, 3);
-//       p1.Wypisz();
-//
-//       ProstokatP1 p2 = new ProstokatP1(-2,  3);
-//        p2.Wypisz();
-//
-//
-        Budynek b1 = new Budynek("WSIZ", 3, LocalDate.of(2010, 1,25), 2010);
-        b1.Wypisz();
-    }
+       ProstokatP1 p1 = new ProstokatP1(2, 3);
+       p1.Wypisz();
 
-//        Gatunek g1 = new Gatunek("Pies domowy", "Canis familiaris", "Udomowiony gatunek ssaka drapieżnego z rodziny psowatych", 37, 37, 74);
-//        g1.wypisz();
-//    }
+       ProstokatP1 p2 = new ProstokatP1(-2,  3);
+       p2.Wypisz();
+
+       Budynek b1 = new Budynek("WSIZ", 3, LocalDate.of(2010, 1,25), 2010);
+       b1.Wypisz();
+
+       Gatunek g1 = new Gatunek("Pies domowy", "Canis familiaris", "Udomowiony gatunek ssaka drapieżnego z rodziny psowatych", 37, 37, 74);
+       g1.wypisz();
+        
+    }
 }
